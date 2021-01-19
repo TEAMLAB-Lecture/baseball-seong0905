3c2e8dc904e6c9d47d5120da7e71be11ffa24678
unknown <seong0905@github.com>
2021-01-19 21:05:56 +0900

    Update my code

--- baseball_game.py
+++ baseball_game.py
@@ -263,35 +263,38 @@ def is_no(one_more_input):
 def main():
     print("Play Baseball")
     user_input = 999
-    random_number = str(get_not_duplicated_three_digit_number())
-    print("Random Number is : ", random_number)
-    # ===Modify codes below=============
-    # 위의 코드를 포함하여 자유로운 수정이 가능함
     game = True
     while(game):
-        user_input = input('Input guess number : ')
-        if user_input == '0':
-            break
-        if is_validated_number(user_input):
-            strikes, balls = get_strikes_or_ball(user_input, random_number)
-            print("Strikes :",strikes,", Balls :",balls)
-            if strikes == 3:
-                while(True):
-                    one_more_input = input('You win, one more(Y/N) ?')
-                    if is_yes(one_more_input):
-                        main()
-                        return
-                    elif is_no(one_more_input):
-                        break
-                    elif one_more_input == '0':
-                        break
-                    else:
-                        print("Wrong Input")
+        random_number = str(get_not_duplicated_three_digit_number())
+        print("Random Number is : ", random_number)
+        # ===Modify codes below=============
+        # 위의 코드를 포함하여 자유로운 수정이 가능함
+        while(True):
+            user_input = input('Input guess number : ')
+            if user_input == '0':
                 break
-                               
-        else:
-            print("Wrong Input, Input again")
-
+            if is_validated_number(user_input):
+                strikes, balls = get_strikes_or_ball(user_input, random_number)
+                print("Strikes :",strikes,", Balls :",balls)
+                if strikes == 3:
+                    while(True):
+                        one_more_input = input('You win, one more(Y/N) ?')
+                        if is_yes(one_more_input):
+                            game = True
+                            break
+                        elif is_no(one_more_input):
+                            game = False
+                            break
+                        elif one_more_input == '0':
+                            game = False
+                            break
+                        else:
+                            print("Wrong Input")
+                    break
+                                
+            else:
+                print("Wrong Input, Input again")
+        
 
     # ==================================
     print("Thank you for using this program")
